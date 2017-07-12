74cdc962899a508da6a8fd3c2525cda59c7aa497
Isaiah Walker <Walkisai.72@gmail.com>
2017-07-12 11:19:51 -0700

    finished the TODO's as Isaiah(driver) and Isaac(nav)

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -5,26 +5,26 @@ var articles = [];
 function Article (rawDataObj) {
   // TODO: Done. See lines 8-12; Use the JS object passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
-  this.title = rawData.title;
-  this.category = rawData.category;
-  this.author = rawData.author;
-  this.url = rawData.authorUrl;
-  this.body = rawData.body;
-  this.publishedOn = rawData.publishedOn;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.url = rawDataObj.authorUrl;
+  this.body = rawDataObj.body;
+  this.publishedOn = rawDataObj.publishedOn;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template.
+  /* TODO: Done: Removed class of cloned article. This cloned article still has a class of template.
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
   sure we're not accidentally hiding our cloned article! */
-  $newArticle.removeClass();
+  $($newArticle).removeClass().addClass(this.title);
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest
+  /* TODO: Done. See line 35-39. Now use jQuery traversal and setter methods to fill in the rest
   of the current template clone with properties from this particular Article instance.
   We need to fill in:
     1. author name,
@@ -32,6 +32,11 @@ Article.prototype.toHtml = function() {
     3. article title,
     4. article body, and
     5. publication date. */
+    $('.template').find('h1').html(this.title);
+    $('.template').find('address').html(this.author);
+    $('.template').find('href').html(this.url);
+    $('.template').find('time').html(this.publishedOn);
+    $('.template').find('section').html(this.body);
 
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
